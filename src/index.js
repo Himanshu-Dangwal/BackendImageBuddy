c6031d016c91f78c261897e26b6747004f3d0fb5
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2024-12-02 19:43:33 -0800

    Final changes before deployment

--- src/index.js
+++ src/index.js
@@ -7,7 +7,13 @@ const authRoutes = require('../routes/authRoutes');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5050;
+// const PORT = process.env.PORT || 5050
+
+const PORT = process.env.PORT
+
+// const dbUrl = process.env.DB_URL || process.env.MONGODB_URI
+// const dbUrl = process.env.dbUrl
+const dbUrl = process.env.DB_URL
 
 app.use(cors());
 app.use(express.json()); // Middleware to parse JSON bodies
@@ -16,10 +22,16 @@ app.use(express.json()); // Middleware to parse JSON bodies
 app.use('/api/auth', authRoutes);
 
 mongoose
-    .connect(process.env.MONGODB_URI)
+    .connect(dbUrl)
     .then(() => {
         app.listen(PORT, () => {
+            console.log(dbUrl);
             console.log(`Server running on http://localhost:${PORT}`);
         });
     })
     .catch((err) => console.error('Error connecting to MongoDB:', err));
+
+
+app.get("/", (req, res) => {
+    res.send("This is Image Buddy Application");
+})
